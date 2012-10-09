f0fd02d56e28c402389827a149db02ede9e84ce9
Robert Bittle <guywithnose@gmail.com>
2012-10-09 16:27:47 -0400

    Add the preamble to the constitution

---
+++ file.php
@@ -0,0 +1,15 @@
+<?php
+
+echo <<<'NOWDOC'
+
+We the People of the United States,
+in Order to from a more perfect Union,
+establish Justice, insure domestic Tranquility,
+provide for the common defence,
+promote the genral welfare,
+and secure the Blessings of Liberty
+to ourselves and our Posterity,
+do ordain and establish this Constitution
+for the United States of America.
+
+NOWDOC;
