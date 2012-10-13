aaa0909e59a3ba414c51189bd23d8f91716e83ea
Robert Bittle <robert.bittle@dominionenterprises.com>
2012-10-13 00:50:44 -0400

    Capitalize welfare

--- file.php
+++ file.php
@@ -6,7 +6,7 @@ We the People of the United States,
 in Order to from a more perfect Union,
 establish Justice, insure domestic Tranquility,
 provide for the common defence,
-promote the general welfare,
+promote the general Welfare,
 and secure the Blessings of Liberty
 to ourselves and our Posterity,
 do ordain and establish this Constitution
