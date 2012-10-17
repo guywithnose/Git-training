9f1e49abfbbb08b63884de6bcd247f84d598c440
Robert Bittle <guywithnose@gmail.com>
2012-10-17 15:05:38 -0400

    Change for to four

--- file.php
+++ file.php
@@ -2,7 +2,7 @@
 
 echo <<<'NOWDOC'
 
- For score and seven years ago our
+ Four score and seven years ago our
 fathers brought forth on this continent,
 a new nation, conceived in Liberty,
 and dedicated to the proposition that all men are created equal.
